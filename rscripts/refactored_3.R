0db935e28d06ee4cf04a5ee01223f73dc8862616
Jacek Kopecky <jacek@jacek.cz>
2019-02-22 16:48:02 +0000

    more refactoring
    Co-Authored-By: Dillon O'Shea <dillon.oshea@myport.ac.uk>
    Co-Authored-By: Fionnlagh Plant <finlay.plant@myport.ac.uk>

--- rscripts/refactored_3.R
+++ rscripts/refactored_3.R
@@ -1,8 +1,8 @@
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
 dd <- as.integer(args[1])
 
-input = data.frame(
+input <- data.frame(
   colo = args[2],
   div = args[3],
   dis = args[4],
@@ -49,21 +49,26 @@ if (length(wells_over_90) == 0){ arsenic_over_90 = 0 }
 as_mean_over_90 <- mean(arsenic_over_90)
 
 #the following three pieces of code decide the output message
-Pol_stat<-if (((as_median_under_90)>20) && (((as_median_under_90)<=50))){"likely to be Polluted"
-} else if (((as_median_under_90)>50) && (((as_median_under_90)<=200))){"likely to be HIGHLY Polluted"
-} else if ((as_median_under_90)>200){"likely to be SEVERELY Polluted"
-}else {"likely to be arsenic-safe"}
+pollution_status_under_90 <-
+  if ((as_median_under_90>20) && (as_median_under_90<=50)) {
+    "likely to be Polluted"
+  } else if ((as_median_under_90>50) && (as_median_under_90<=200)) {
+    "likely to be HIGHLY Polluted"
+  } else if (as_median_under_90>200) {
+    "likely to be SEVERELY Polluted"
+  } else {
+    "likely to be arsenic-safe"
+  }
 
-chem_test <- if ((as_max_under_90>=0)&&(as_max_under_90<=100)){"and concentration may be around"
-}else {", a chemical test is needed as concentration can be high, ranging around"
-}
+chem_test <-
+  if ((as_max_under_90>=0) && (as_max_under_90<=100)) {
+    "and concentration may be around"
+  }else {
+    ", a chemical test is needed as concentration can be high, ranging around"
+  }
 
-#Pol_90 is declared here
-if (as_mean_over_90 >= 50) {Pol_90 <- "Your tubewell is highly likely to be Polluted."
-} else if (as_mean_over_90 < 50) {Pol_90 <- "Your tubewell may be arsenic-safe."
-}
 
-# rounding up to the next 10
+# rounding up to the next round.val (e.g. to the next 10)
 round.choose <- function(x, round.val, dir = 1) {
   if(dir == 1) {  ##ROUND UP
     x + (round.val - x %% round.val)
@@ -73,34 +78,41 @@ round.choose <- function(x, round.val, dir = 1) {
     }
   }
 }
-#-----------------
-
-#Assessment based on basement colour
 
 
-
-warning_severity = ''
-nitrate_warning = ''
 
 if (length(wells_in_area) > 0){
-	flood_warning = ''
-	if ((dd <= 15) && (input$flood == 'No')){ flood_warning = 'but may be vulnerable to nitrate and pathogens' }
 
-	if ((input$colo == 'Black' | input$utensil == "No colour change to slightly blackish")) {
-	  warning_severity = ''
-	  if (dd > 150) { warning_severity = 'HIGHLY' }
-	  paste ("Your tubewell is", warning_severity, "likely to be arsenic-safe", flood_warning)
-	} else if ((input$colo == 'Red' | input$utensil == "Red")) {
-	  if ((dd <= 15) && (input$colo == "Red") && (as_median_under_90 >= 20)) {
-	    paste ("in", Pol_stat, chem_test, round.choose (lower_quantile_under_90, 10,1), "to", round.choose (upper_quantile_under_90, 10,1),"µg/L ")
-	  } else if (dd < 90){
-		paste ("Your tubewell is", Pol_stat, chem_test, round.choose (lower_quantile_under_90, 10,1), "to", round.choose (upper_quantile_under_90, 10,1),"µg/L ") #flood_warning removed temporarily to ensure comparable output to the original.
-	  } else if (dd <=150) {
-			paste (Pol_90)
-	  } else {
-			paste ("Your tubewell is HIGHLY likely to be arsenic-safe")
-	  }
-	}
+  flood_warning <-
+    if ((dd <= 15) && (input$flood == 'No')) {
+      'but may be vulnerable to nitrate and pathogens'
+    } else {
+      ''
+    }
+
+  if ((input$colo == 'Black' | input$utensil == "No colour change to slightly blackish")) {
+
+    warning_severity = if (dd > 150) 'HIGHLY' else ''
+
+    paste ("Your tubewell is", warning_severity, "likely to be arsenic-safe", flood_warning)
+
+  } else if ((input$colo == 'Red' | input$utensil == "Red")) {
+
+    if ((dd <= 15) && (input$colo == "Red") && (as_median_under_90 >= 20)) {
+      paste ("in", pollution_status_under_90, chem_test, round.choose (lower_quantile_under_90, 10,1), "to", round.choose (upper_quantile_under_90, 10,1),"µg/L ")
+    } else if (dd < 90){
+      paste ("Your tubewell is", pollution_status_under_90, chem_test, round.choose (lower_quantile_under_90, 10,1), "to", round.choose (upper_quantile_under_90, 10,1),"µg/L ")
+    } else if (dd <=150) {
+      if (as_mean_over_90 >= 50) {
+        paste ("Your tubewell is highly likely to be Polluted.")
+      } else if (as_mean_over_90 < 50) {
+        paste ("Your tubewell may be arsenic-safe.")
+      }
+    } else {
+      paste ("Your tubewell is HIGHLY likely to be arsenic-safe")
+    }
+
+  }
 } else {
-    paste("We are unable to assess your tubewell with the information you supplied, please fill all the sections")
+  paste("We are unable to assess your tubewell with the information you supplied, please fill all the sections")
 }
