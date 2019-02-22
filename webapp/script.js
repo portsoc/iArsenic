886c8fe5dc17819941703fc6e8804fa9e0e9ed40
CybrChick007 <37068838+CybrChick007@users.noreply.github.com>
2019-02-22 17:47:10 +0000

    Basic function attempts as we move into JS
    Co-Authored-By: Dillon  <TingeOGinge@users.noreply.github.com>

--- webapp/script.js
+++ webapp/script.js
@@ -1,6 +1,6 @@
-function populateDropdown(elemStr, arr){
+function populateDropdown(elemStr, arr) {
   const dropdown = document.getElementById(elemStr);
-  for (let i = 0; i < testList.length; i += 1){
+  for (let i = 0; i < testList.length; i += 1) {
     const opt = document.createElement("option");
     opt.value = arr[i];
     opt.text = arr[i];
@@ -12,3 +12,24 @@ function populateDropdown(elemStr, arr){
 const testList = ['a', 'b', 'c', 'd', 'e', 'f']
 const elem = 'district_dd';
 populateDropdown(elem, testList);
+
+
+function pollution_status_u_90() {
+  if ((median_under90 > 20) && (median_under90 <= 50)) {
+    return "is likely to be Polluted";
+  } else if ((median_under90 > 50) && (median_under90 <= 200)) {
+    return "is likely to be HIGHLY Polluted";
+  } else if (median_under90 > 200) {
+    return "likely to be SEVERLY Polluted"
+  } else {
+    return "likely to be arsenic-safe"
+  }
+}
+
+function chem_test() {
+  if ((max_under90 >= 0) && (max_under90 <= 100)) {
+    return "and concentration may be around"
+  } else {
+    return ", a chemical test is needed as concentration can be high, ranging around"
+  }
+}
