76f03ea129b50e9dccf1a329708410697e32cb2a
Jacek Kopecky <jacek@jacek.cz>
2019-03-01 15:46:51 +0000

    detailed plan
    Co-Authored-By: Fionnlagh Plant <finlay.plant@myport.ac.uk>
    Co-Authored-By: Dillon O'Shea <dillon.oshea@myport.ac.uk>

--- preprocessing/index.js
+++ preprocessing/index.js
@@ -16,8 +16,7 @@ console.log(records[100]);
 
 
 /*
-for every area, compute the following:
-for now, just to the level of upazila
+for every area, on every level, compute the following:
 
 as_median_under_90   (<90)
 as_max_under_90
@@ -26,4 +25,101 @@ upper_quantile_under_90
 as_mean_over_90  (>=90)
 
 
+have a hash of divisions
+
+const divisions = {
+  wells_under_90: [],
+  wells_over_90: [],
+
+  as_median_under_90,
+  as_max_under_90,
+  lower_quantile_under_90,
+  upper_quantile_under_90,
+  as_mean_over_90,
+
+  districts: {
+    wells_under_90: [],
+    wells_over_90: [],
+
+    as_median_under_90,
+    as_max_under_90,
+    lower_quantile_under_90,
+    upper_quantile_under_90,
+    as_mean_over_90,
+
+    upazilas: {
+      wells_under_90: [],
+      wells_over_90: [],
+
+      as_median_under_90,
+      as_max_under_90,
+      lower_quantile_under_90,
+      upper_quantile_under_90,
+      as_mean_over_90,
+
+      unions: {
+        wells_under_90: [],
+        wells_over_90: [],
+
+        as_median_under_90,
+        as_max_under_90,
+        lower_quantile_under_90,
+        upper_quantile_under_90,
+        as_mean_over_90,
+      }
+    }
+  }
+}
+
+const divisions = {}
+
+for each record
+  if it's missing any data, skip it
+  if we don't know the division (!(record.Division in divisions)), add a new object for it
+    divisons[record.Division] = {
+      wells_under_90: [],
+      wells_over_90: [],
+      districts: {},
+    }
+  in the division, add the well in wells_under_90[] or wells_over_90[]
+  if we don't know the district, add a new object for it
+  in the district, add the well in wells_under_90[] or wells_over_90[]
+  if we don't know the upazila, add a new object for it
+  in the upazila, add the well in wells_under_90[] or wells_over_90[]
+  if we don't know the union, add a new object for it
+  in the union, add the well in wells_under_90[] or wells_over_90[]
+
+for each division
+  sort the arrays
+  if we don't have enough data under 90
+    complain
+  else
+    compute as_median_under_90   (<90)
+    compute as_max_under_90
+    compute lower_quantile_under_90
+    compute upper_quantile_under_90
+  if we don't have enough data over 90
+    complain
+  else
+    compute as_mean_over_90  (>=90)
+
+for each district
+  sort the arrays
+  if we don't have enough data under 90
+    take the computations from the division or complain
+  else
+    compute as_median_under_90   (<90)
+    compute as_max_under_90
+    compute lower_quantile_under_90
+    compute upper_quantile_under_90
+  if we don't have enough data over 90
+    take the computations from the division or complain
+  else
+    compute as_mean_over_90  (>=90)
+
+similar for upazila, union
+
+create a function that given all the parameters spits out an estimate
+
+replicate test.sh in javascript
 */
