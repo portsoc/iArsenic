87ad473c96723f21c5adc6e4d6f1e79118a1e6a4
Jacek Kopecky <jacek@jacek.cz>
2019-03-02 16:12:03 +0000

    move common testing code into test-runtests.sh

---
+++ rscripts/test-runtests.sh
@@ -0,0 +1,50 @@
+if which gecho > /dev/null
+then
+  # on mac, gecho is a better echo
+  alias echo=gecho
+fi
+
+scriptname=${1:-reference.R}
+
+echo using script "$scriptname"
+
+function runtests
+{
+  for depth in 20 60 100 200  # dropped 10 because we don't do flood
+                              # and we don't have utensil yet
+  do
+    for colour in Red Black
+    do
+      echo -n \"$div\" \"$dis\" \"$upa\" \"$uni\" $depth $colour '-' '-' ' '
+      Rscript "$scriptname" "$div" "$dis" "$upa" "$uni" $depth $colour - -  2>/dev/null # hide warnings
+      echo
+    done
+    for utensil in "Red" "No colour change to slightly blackish"
+    do
+      colour="none"
+      utensilshort=${utensil:0:2}
+      echo -n \"$div\" \"$dis\" \"$upa\" \"$uni\" $depth $colour \"$utensilshort\" '-' ' '
+      Rscript "$scriptname" "$div" "$dis" "$upa" "$uni" $depth $colour "$utensil" - 2>/dev/null # hide warnings
+      echo
+    done
+  done
+
+  depth=10
+  for flooding in "Yes" "No"
+  do
+    for colour in Red Black
+    do
+      echo -n \"$div\" \"$dis\" \"$upa\" \"$uni\" $depth $colour '-' \"$flooding\" ' '
+      Rscript "$scriptname" "$div" "$dis" "$upa" "$uni" $depth $colour - "$flooding" 2>/dev/null # hide warnings
+      echo
+    done
+    for utensil in "Red" "No colour change to slightly blackish"
+    do
+      colour="none"
+      utensilshort=${utensil:0:2}
+      echo -n \"$div\" \"$dis\" \"$upa\" \"$uni\" $depth $colour \"$utensilshort\" \"$flooding\"  ' '
+      Rscript "$scriptname" "$div" "$dis" "$upa" "$uni" $depth $colour "$utensil" "$flooding" 2>/dev/null # hide warnings
+      echo
+    done
+  done
+}
