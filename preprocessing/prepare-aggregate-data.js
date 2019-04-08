03a79e98d80bc53cb690206798524e178b3111ac
Jacek Kopecky <jacek@jacek.cz>
2019-04-08 17:20:57 +0100

    add aggregate data
    Co-Authored-By: Dillon O'Shea <dillon.oshea@myport.ac.uk>
    Co-Authored-By: Fionnlagh Plant <finlay.plant@myport.ac.uk>

---
+++ preprocessing/prepare-aggregate-data.js
@@ -0,0 +1,71 @@
+/*
+
+This script generates a JSON representation of the location hierarchy including pre-processed arsenic level data
+which looks like this:
+
+[
+  division: '..',
+
+  as_median_under_90: ...,
+  as_max_under_90: ...,
+  lower_quantile_under_90: ...,
+  upper_quantile_under_90: ...,
+  as_mean_over_90: ...,
+
+  districts: [
+    district: '..',
+    stats as above,
+    upazilas: [
+      upazila: '..',
+      stats as above,
+      unions: [
+        union: '..',
+        stats as above,
+      ]
+    ]
+  ]
+]
+```
+
+*/
+
+const csvLoader = require('./load-csv');
+
+console.debug = () => {};
+const data = csvLoader();
+
+// get the admin region hierarchy only
+
+function extractStats(data, hierarchyPath) {
+  const retval = [];
+  for (const item of Object.keys(data)) {
+    const dataObj = data[item];
+    const hierarchyObj = {
+      [hierarchyPath[0]]: item,
+
+      med: dataObj.as_median_under_90,
+      max: dataObj.as_max_under_90,
+      low: dataObj.lower_quantile_under_90,
+      upp: dataObj.upper_quantile_under_90,
+      mo9: dataObj.as_mean_over_90,
+    };
+    if (hierarchyPath.length > 1) {
+      const subData = dataObj[hierarchyPath[1] + 's'];
+      hierarchyObj[hierarchyPath[1] + 's'] = extractStats(subData, hierarchyPath.slice(1));
+    }
+    retval.push(hierarchyObj);
+  }
+  retval.sort(compareByProperty(hierarchyPath[0]));
+  return retval;
+}
+
+function compareByProperty(prop) {
+  return (a, b) => {
+    if (a[prop] < b[prop]) return -1;
+    if (a[prop] === b[prop]) return 0;
+    return 1;
+  };
+}
+
+const hierarchy = extractStats(data, ['division', 'district', 'upazila', 'union']);
+console.log(JSON.stringify(hierarchy));
