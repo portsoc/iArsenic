afefc15c828cc710f7c5a1b853e0be0285bf50ee
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2019-06-14 10:46:10 +0100

    CSV preprocessing now includes 3 strata in line with model 3

--- preprocessing/load-csv.js
+++ preprocessing/load-csv.js
@@ -19,6 +19,7 @@ function readTheCSVFile() {
 }
 
 // prepare the location-based data hierarchy if the location is new
+// 3 Strata of wells by depth 'd': shallow(d<90), med(90<=d<150), deep(150<=d)
 function extractLocations(records) {
   const divisions = {};
 
@@ -30,8 +31,9 @@ function extractLocations(records) {
 
     if (!(r.Division in divisions)) {
       divisions[r.Division] = {
-        wells_under_90: [],
-        wells_over_90: [],
+        wells_shallow: [],
+        wells_med: [],
+        wells_deep: [],
         districts: {},
         name: r.Division,
       };
@@ -40,8 +42,9 @@ function extractLocations(records) {
 
     if (!(r.District in division.districts)) {
       division.districts[r.District] = {
-        wells_under_90: [],
-        wells_over_90: [],
+        wells_shallow: [],
+        wells_med: [],
+        wells_deep: [],
         upazilas: {},
         name: r.District,
         parent: division,
@@ -51,8 +54,9 @@ function extractLocations(records) {
 
     if (!(r.Upazila in district.upazilas)) {
       district.upazilas[r.Upazila] = {
-        wells_under_90: [],
-        wells_over_90: [],
+        wells_shallow: [],
+        wells_med: [],
+        wells_deep: [],
         unions: {},
         name: r.Upazila,
         parent: district,
@@ -62,8 +66,9 @@ function extractLocations(records) {
 
     if (!(r.Union in upazila.unions)) {
       upazila.unions[r.Union] = {
-        wells_under_90: [],
-        wells_over_90: [],
+        wells_shallow: [],
+        wells_med: [],
+        wells_deep: [],
         name: r.Union,
         parent: upazila,
       };
@@ -109,56 +114,86 @@ function fillArsenicData(divisions, records) {
     const union = upazila.unions[r.Union];
 
     if (Number(r.Depth) < 90) {
-      division.wells_under_90.push(Number(r.Arsenic));
-      district.wells_under_90.push(Number(r.Arsenic));
-      upazila.wells_under_90.push(Number(r.Arsenic));
-      union.wells_under_90.push(Number(r.Arsenic));
+      division.wells_shallow.push(Number(r.Arsenic));
+      district.wells_shallow.push(Number(r.Arsenic));
+      upazila.wells_shallow.push(Number(r.Arsenic));
+      union.wells_shallow.push(Number(r.Arsenic));
+    } else if (Number(r.Depth) < 150){
+      division.wells_med.push(Number(r.Arsenic));
+      district.wells_med.push(Number(r.Arsenic));
+      upazila.wells_med.push(Number(r.Arsenic));
+      union.wells_med.push(Number(r.Arsenic));
     } else {
-      division.wells_over_90.push(Number(r.Arsenic));
-      district.wells_over_90.push(Number(r.Arsenic));
-      upazila.wells_over_90.push(Number(r.Arsenic));
-      union.wells_over_90.push(Number(r.Arsenic));
+      division.wells_deep.push(Number(r.Arsenic));
+      district.wells_deep.push(Number(r.Arsenic));
+      upazila.wells_deep.push(Number(r.Arsenic));
+      union.wells_deep.push(Number(r.Arsenic));
     }
   }
 }
 
 function computeWellStats(location) {
   // sort the arsenic concentration data arrays
-  location.wells_under_90.sort(numericalCompare);
-  location.wells_over_90.sort(numericalCompare);
+  location.wells_shallow.sort(numericalCompare);
+  location.wells_med.sort(numericalCompare);
+  location.wells_deep.sort(numericalCompare);
 
-  // if we don't have enough data under 90
+  // if we don't have enough shallow well data (d<90)
   //   take the computations from the parent or complain
-  if (location.wells_under_90.length < MIN_DATA_COUNT) {
+  if (location.wells_shallow.length < MIN_DATA_COUNT) {
     if (!location.parent) {
       console.debug(`Division ${location.name} does not have enough wells under 90`);
     } else {
-      location.as_median_under_90 = location.parent.as_median_under_90;
-      location.as_max_under_90 = location.parent.as_max_under_90;
-      location.lower_quantile_under_90 = location.parent.lower_quantile_under_90;
-      location.upper_quantile_under_90 = location.parent.upper_quantile_under_90;
+      location.as_median_shallow = location.parent.as_median_shallow;
+      location.as_max_shallow = location.parent.as_max_shallow;
+      location.lower_quantile_shallow = location.parent.lower_quantile_shallow;
+      location.upper_quantile_shallow = location.parent.upper_quantile_shallow;
     }
   } else {
-    // we do have enough data under 90
-    location.as_median_under_90 = stats.median(location.wells_under_90);
-    location.as_max_under_90 = stats.max(location.wells_under_90);
-    location.lower_quantile_under_90 = stats.quantile(location.wells_under_90, 0.1);
-    location.upper_quantile_under_90 = stats.quantile(location.wells_under_90, 0.9);
+    // we do have enough data
+    location.as_median_shallow = stats.median(location.wells_shallow);
+    location.as_max_shallow = stats.max(location.wells_shallow);
+    location.lower_quantile_shallow = stats.quantile(location.wells_shallow, 0.1);
+    location.upper_quantile_shallow = stats.quantile(location.wells_shallow, 0.9);
   }
 
-  // if we don't have enough data over 90
+  // if we don't have enough med well data (90<=d<150)
   //   take the computations from the parent or complain
-  if (location.wells_over_90.length < MIN_DATA_COUNT) {
+  if (location.wells_med.length < MIN_DATA_COUNT) {
     if (!location.parent) {
       console.debug(`Division ${location.name} does not have enough wells under 90`);
     } else {
-      location.as_mean_over_90 = location.parent.as_mean_over_90;
+      location.as_median_med = location.parent.as_median_med;
+      location.as_max_med = location.parent.as_max_med;
+      location.lower_quantile_med = location.parent.lower_quantile_med;
+      location.upper_quantile_med = location.parent.upper_quantile_med;
     }
   } else {
-    // we do have enough data over 90
-    location.as_mean_over_90 = stats.mean(location.wells_over_90);
+    // we do have enough data
+    location.as_median_med = stats.median(location.wells_med);
+    location.as_max_med = stats.max(location.wells_med);
+    location.lower_quantile_med = stats.quantile(location.wells_med, 0.1);
+    location.upper_quantile_med = stats.quantile(location.wells_med, 0.9);
   }
-}
+  
+  // if we don't have enough deep well data (150<=d)
+  //   take the computations from the parent or complain
+  if (location.wells_deep.length < MIN_DATA_COUNT) {
+    if (!location.parent) {
+      console.debug(`Division ${location.name} does not have enough wells under 90`);
+    } else {
+      location.as_median_deep = location.parent.as_median_deep;
+      location.as_max_deep = location.parent.as_max_deep;
+      location.lower_quantile_deep = location.parent.lower_quantile_deep;
+      location.upper_quantile_deep = location.parent.upper_quantile_deep;
+    }
+  } else {
+    // we do have enough data
+    location.as_median_deep = stats.median(location.wells_deep);
+    location.as_max_deep = stats.max(location.wells_deep);
+    location.lower_quantile_deep = stats.quantile(location.wells_deep, 0.1);
+    location.upper_quantile_deep = stats.quantile(location.wells_deep, 0.9);
+  }
 
 function numericalCompare(a, b) {
   return a - b;
