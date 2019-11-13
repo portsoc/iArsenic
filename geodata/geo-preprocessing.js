a7d47571a04b7261d60e1a3262c6e6d4fce1b890
Lewis Newton <lewisknewton@hotmail.com>
2019-11-13 16:33:47 +0000

    calculate centroids and distances separately for regions
    Co-Authored-By: Dillon O'Shea <dillon.oshea@myport.ac.uk>

--- geodata/geo-preprocessing.js
+++ geodata/geo-preprocessing.js
@@ -1,27 +1,29 @@
 const d3 = require('d3');
 const topojson = require('topojson');
-const map = require('./maps/dist/div/div-c005-s010-vw-pr.json');
+const ADMINISTRATIVE_LEVEL = 'uni';
+const map = require(`./maps/dist/${ADMINISTRATIVE_LEVEL}/${ADMINISTRATIVE_LEVEL}-c005-s010-vw-pr.json`);
 const topo = topojson.feature(map, map['objects']['map']);
 
 function runDistanceTests() {
-  const prop = 'div';
+  const prop = ADMINISTRATIVE_LEVEL;
   const closestRegion = {};
 
   console.log(`The topo centroid is ${d3.geoCentroid(topo)}`);
 
   for (const selectedRegion of topo.features) {
+    selectedRegion.centroid = d3.geoCentroid(selectedRegion);
+  }
+
+  for (const selectedRegion of topo.features) {
     closestRegion.distance = Infinity;
 
     let selectedRegionName = selectedRegion.properties[prop];
-    let selectedRegionCentroid = d3.geoCentroid(selectedRegion);
 
     for (const feature of topo.features) {
       let regionName = feature.properties[prop];
 
       if (feature !== selectedRegion) {
-        let regionCentroid = d3.geoCentroid(feature);
-
-        let currentDistance = d3.geoDistance(selectedRegionCentroid, regionCentroid);
+        let currentDistance = d3.geoDistance(feature.centroid, selectedRegion.centroid);
 
         if (currentDistance < closestRegion.distance) {
           closestRegion[prop] = regionName;
