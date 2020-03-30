bb931755b5f8b6bd644f632869e7d3a1f379a3af
Jacek Kopecky <jacek@jacek.cz>
2020-03-30 22:37:41 +0100

    add name correction

---
+++ geodata/name-corrections.js
@@ -0,0 +1,23 @@
+const corrections = {
+  'Sylhet#Sunamganj#Dharampasha#Dakshin  Sukhairrajapur': [
+    'Sylhet', 'Sunamganj', 'Dharampasha', 'Dakshin Sukhairrajapur' ],
+};
+
+function combineName(o) {
+  return `${o.div}#${o.dis}#${o.upa}#${o.uni}`;
+}
+
+function replaceName(o, arr) {
+  [ o.div, o.dis, o.upa, o.uni ] = arr;
+}
+
+function correctAreaName(area) {
+  const correction = corrections[combineName(area)];
+  if (correction) {
+    replaceName(area, correction);
+  }
+}
+
+module.exports = {
+  correct: correctAreaName,
+};
