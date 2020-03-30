3243d857319526527fd1b3df0c2c6af7fb1bf8ae
Jacek Kopecky <jacek@jacek.cz>
2020-03-30 21:42:09 +0100

    :art: a bit of cleanup

--- preprocessing/lib/centroids.js
+++ preprocessing/lib/centroids.js
@@ -1,32 +1,37 @@
 const getSimpleList = require('../../geodata/create-geo-list.js');
 const d3 = require('d3');
-const {round1} = require('./stats.js');
 const RADIUS = 6378.137;
 
-function computeNearbyWells(data) {
+function computeNearbyWells() {
+  const data = getSimpleList();
+
+  for (const area of data) {
+    area.nearbyAreas = [];
+  }
+
   for (let i = 0; i < data.length; i++) {
     for (let j = i + 1; j < data.length; j++) {
-      let distance = round1(d3.geoDistance(data[i].centroid, data[j].centroid) * RADIUS);
+      const distance = d3.geoDistance(data[i].centroid, data[j].centroid) * RADIUS;
 
-      if (distance <= 100) {
-        data[i].nearbyAreas.push({
-          km: distance,
-          uni: data[j].uni,
-        });
+      data[i].nearbyAreas.push({
+        km: distance,
+        area: data[j],
+      });
 
-        data[j].nearbyAreas.push({
-          km: distance,
-          uni: data[i].uni,
-        });
-      }
+      data[j].nearbyAreas.push({
+        km: distance,
+        area: data[i],
+      });
     }
+  }
 
-    data[i].nearbyAreas = data[i].nearbyAreas.sort((a, b) => a.distance - b.distance);
+  for (const area of data) {
+    area.nearbyAreas.sort((a, b) => a.km - b.km);
   }
 
   return data;
 }
 
-computeNearbyWells(getSimpleList());
+computeNearbyWells();
 
 module.exports = computeNearbyWells;
