7a24cf6e78bfe893e176f1f7e849a1fa343d0c00
Jacek Kopecky <jacek@jacek.cz>
2020-03-31 15:52:18 +0100

    commit estimator fixes in testing benchmark data
    Co-Authored-By: Dillon  <TingeOGinge@users.noreply.github.com>
    Co-Authored-By: Lewis Newton <lewisknewton@users.noreply.github.com>

--- preprocessing/tests/benchmark-data/model1/disabled_29k-original/estimator.js
+++ preprocessing/tests/benchmark-data/model1/disabled_29k-original/estimator.js
@@ -36,7 +36,7 @@ function produceEstimate(divisions, div, dis, upa, uni, depth, colour, utensil, 
     const warningSeverity = (depth > 150) ? 'HIGHLY ' : '';
 
     const floodWarning =
-      (depth <= 15 && flood === 'No')
+      (depth <= 15.3 && flood === 'No')
         ? ' but may be vulnerable to nitrate and pathogens'
         : '';
 
