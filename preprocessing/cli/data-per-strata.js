2ffdec1b0ad7c14ec58d4309b2d0bb342f041307
TxJson <nylahk00@gmail.com>
2020-07-07 16:19:16 +0100

    refactoring data-per-strata.js

--- preprocessing/cli/data-per-strata.js
+++ preprocessing/cli/data-per-strata.js
@@ -1,53 +1,63 @@
 const csvLoader = require('../lib/load-data');
 const cli = require('../lib/cli-common');
 
+const STRATA = [
+  { min: 0, max: Infinity, header: 'depth 0+' },
+  { min: 0, max: 90, header: 'depth 0-90' },
+  { min: 90, max: Infinity, header: 'depth 90+' },
+  { min: 90, max: 150, header: 'depth 90-150'},
+  { min: 150, max: Infinity, header: 'depth 150+' },
+  { min: 0, max: 15.3, header: 'depth 0-15.3' },
+  { min: 15.3, max: 45, header: 'depth 15.3-45' },
+  { min: 45, max: 65, header: 'depth 45-65' },
+  { min: 65, max: 90, header: 'depth 65-90' },
+];
+
 function main(options) {
   // get data from CSV files
   const data = csvLoader(options.paths);
 
   // define array to hold rows of CSV file and push column headers
+
   const records = [];
   const headers = [
     'division',
     'district',
     'upazila',
-    'union',
-    'depth 0-90',
-    'depth 90+',
-    'depth 90-150',
-    'depth 150+',
-    'depth 0-15.3',
-    'depth 15.3-45',
-    'depth 45-65',
-    'depth 65-90',
+    'union'
   ];
+
+  // Adds all the stratum headers to the headers array
+  for (const stratum of STRATA) {
+    headers.push(stratum.header);
+  }
+
   records.push(headers);
 
   // loop through each well
-  for (const division of Object.keys(data)) {
-    const divisionObj = data[division];
-    initStratas(divisionObj);
-    for (const district of Object.keys(data[division].districts)) {
-      const districtObj = data[division].districts[district];
-      initStratas(districtObj);
-      for (const upazila of Object.keys(data[division].districts[district].upazilas)) {
-        const upazilaObj = data[division].districts[district].upazilas[upazila];
-        initStratas(upazilaObj);
-        for (const union of Object.keys(data[division].districts[district].upazilas[upazila].unions)) {
-          const unionObj = data[division].districts[district].upazilas[upazila].unions[union];
-          initStratas(unionObj);
-          for (const well of data[division].districts[district].upazilas[upazila].unions[union].wells) {
-            countStratas(divisionObj, districtObj, upazilaObj, unionObj, well);
+  for (const div of Object.keys(data)) {
+    const divObj = data[div];
+    initStratas(divObj);
+    for (const dis of Object.keys(data[div].districts)) {
+      const disObj = data[div].districts[dis];
+      initStratas(disObj);
+      for (const upa of Object.keys(data[div].districts[dis].upazilas)) {
+        const upaObj = data[div].districts[dis].upazilas[upa];
+        initStratas(upaObj);
+        for (const uni of Object.keys(data[div].districts[dis].upazilas[upa].unions)) {
+          const uniObj = data[div].districts[dis].upazilas[upa].unions[uni];
+          initStratas(uniObj);
+          for (const well of data[div].districts[dis].upazilas[upa].unions[uni].wells) {
+            countStratas([divObj, disObj, upaObj, uniObj], well);
           }
-          pushRecord(records, division, district, upazila, union, unionObj);
+          pushRecord(records, div, dis, upa, uni, uniObj);
         }
-        pushRecord(records, division, district, upazila, '###', upazilaObj);
+        pushRecord(records, div, dis, upa, '###', upaObj);
       }
-      pushRecord(records, division, district, '###', '###', districtObj);
+      pushRecord(records, div, dis, '###', '###', disObj);
     }
-    pushRecord(records, division, '###', '###', '###', divisionObj);
+    pushRecord(records, div, '###', '###', '###', divObj);
   }
-
   // write records to csv file
   for (const record of records) {
     record.join(',');
@@ -56,56 +66,25 @@ function main(options) {
   console.log(contents);
 }
 
-function pushRecord(records, division, district, upazila, union, wellCountObj) {
-  records.push([
-    division,
-    district,
-    upazila,
-    union,
-    wellCountObj.zeroToNinety,
-    wellCountObj.ninetyPlus,
-    wellCountObj.ninetyToOneFifty,
-    wellCountObj.oneFiftyPlus,
-    wellCountObj.zeroToFifteenPointThree,
-    wellCountObj.fifteenPointThreeToFourtyFive,
-    wellCountObj.fortyFiveToSixtyFive,
-    wellCountObj.sixtyFiveToNinety,
-  ]);
+function pushRecord(records, div, dis, upa, uni, wellCountObj) {
+  let record = [div, dis, upa, uni];
+  for (const stratum of STRATA) {
+    record.push(wellCountObj[stratum.header]);
+  }
+  records.push(record);
 }
 
-function initStratas(object) {
-  object.zeroToNinety = 0;
-  object.ninetyPlus = 0;
-  object.ninetyToOneFifty = 0;
-  object.oneFiftyPlus = 0;
-  object.zeroToFifteenPointThree = 0;
-  object.fifteenPointThreeToFourtyFive = 0;
-  object.fortyFiveToSixtyFive = 0;
-  object.sixtyFiveToNinety = 0;
-  return object;
-}
-
-function countStratas(divisionObj, districtObj, upazilaObj, unionObj, well) {
-  const tempObj = [divisionObj, districtObj, upazilaObj, unionObj];
-  for (const obj of tempObj) {
-    if (well.depth > 15.3 && well.depth <= 45) obj.fifteenPointThreeToFourtyFive += 1;
-    if (well.depth > 150) obj.oneFiftyPlus += 1;
-    if (well.depth > 45 && well.depth <= 65) obj.fortyFiveToSixtyFive += 1;
-    if (well.depth > 65 && well.depth <= 90) obj.sixtyFiveToNinety += 1;
-    if (well.depth > 90) obj.ninetyPlus += 1;
-    if (well.depth >= 0 && well.depth <= 15.3) obj.zeroToFifteenPointThree += 1;
-    if (well.depth >= 0 && well.depth <= 90) obj.zeroToNinety += 1;
-    if (well.depth >= 90 && well.depth <= 150) obj.ninetyToOneFifty += 1;
+function initStratas(obj) {
+  for (const stratum of STRATA) {
+    obj[stratum.header] = 0;
   }
 }
 
-function countStratasNew(divisionObj, districtObj, upazilaObj, unionObj, well) {
-  const objects = [divisionObj, districtObj, upazilaObj, unionObj];
+function countStratas(objects, well) {
   for (const stratum of STRATA) {
-    if (well.depth >= stratum.min && well.depth < stratum.max) {
-      const prop = propName(stratum);
+    if (well.depth >= stratum.min && well.depth <= stratum.max) {
       for (const obj of objects) {
-        obj[prop] += 1;
+        obj[stratum.header] += 1;
       }
     }
   }
