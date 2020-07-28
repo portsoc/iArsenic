c2caddba1e747d8f67cce0c0f335fbb698de7ac7
TxJson <nylahk00@gmail.com>
2020-07-28 13:41:31 +0100

    r script cleanup

--- preprocessing/rdata-to-csv.r
+++ preprocessing/rdata-to-csv.r
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
 
-# Run this script in the console:
+# Run script in the console:
 # Rscripts [script name] [rdata file/rdata file directory] [output directory]
 # [output directory] is not necessary if the output is in the same directory
 
 # Pass directories/files as arguments when running script
-args.path = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly=TRUE)
 
 # Creates the directory if it doesn't exist
 dirExists <- function (dirPath) {
@@ -14,36 +14,46 @@ dirExists <- function (dirPath) {
   }
 }
 
-
+# Convert RData file to CSV and output into current or specified directory
 writeToCsv <- function(filePath, writePath) {
-  file.data <- get(load(filePath)) # Loads the data
-  file.pathName = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(filePath)) # Gets the file name from the path
+  file.data <- get(load(filePath)) # Loads the RData
+
+  # Gets the file name from the path
+  file.name = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(filePath))
 
   # Sets the file output path
   if (!is.na(writePath)) {
-    file.name = paste(writePath, "/", file.pathName, ".csv", sep="")
+    dirExists(writePath)
+    file.path = paste(writePath, "/", file.name, ".csv", sep="") # Set file path and name
   } else {
-    file.name = paste(file.pathName, ".csv", sep="")
+    file.path = paste(file.name, ".csv", sep="") # Set file name
   }
-
-  dirExists(writePath)
-  write.csv(file.data, file=file.name) # Writes the data to a CSV file
-  print(paste("File generated at", file.name))
+  write.csv(file.data, file=file.path) # Writes the data to a CSV file
+  print(paste("File generated at", file.path))
 }
 
+# Convert RData files in directory to CSV
 rDirToCsv <- function(dirPath, writePath) {
+  # Find all RData files
   file.names <- dir(dirPath, pattern="*.RData")
+
+  # Loop through directory files
   for(i in 1:length(file.names)) {
-    # file.name = paste("data/data++/", file.names[i], ".csv", sep="")
-    writeToCsv(paste(dirPath, "/", file.names[i], sep=""), writePath)
-    # file.data <- load(file.names[i], verbose="TRUE")
-    # write.csv(file.data, "test.csv")
+    file.path = paste(dirPath, "/", file.names[i], sep="") # Set file path
+    writeToCsv(file.path, writePath)
   }
 }
 
-# Returns true if a file, false if directory
-if (file_test("-f", args.path[1])) {
-  writeToCsv(args.path[1], args.path[2]) # If it's one file
+
+# Checks if arguments has been passed
+if (!is.na(args[1])) {
+
+  # Returns true if a file, false if a directory
+  if (file_test("-f", args[1])) {
+    writeToCsv(args[1], args[2]) # If it's one file
+  } else {
+    rDirToCsv(args[1], args[2]) # If it's a directory of files
+  }
 } else {
-  rDirToCsv(args.path[1], args.path[2]) # If it's a directory of files
+  print("No arguments passed, stopping script...")
 }
