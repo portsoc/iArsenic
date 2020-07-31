41352a774b634525d6ef8db67677adc6d2619be1
Jacek Kopecky <jacek@jacek.cz>
2020-07-31 16:26:54 +0100

    review

---
+++ preprocessing/data-import/README.md
@@ -0,0 +1,3 @@
+1. we got data in Rdata files with about 1 million wells
+2. rdata-to-csv.r converts that into CSV files for ease of processing
+3. consolidate.csv (misnomer - fixme) puts all that data into a single CSV output in the format of our normal data sets (../data/*.csv)
--- preprocessing/data-import/consolidate-csv.js
+++ preprocessing/data-import/consolidate-csv.js
@@ -1,14 +1,16 @@
+// converts the output of rdata-to-csv.r
+
 // Run script in terminal:
 // node [script name/path] [data directory] [output directory]
 // output-directory is not essential but will output to script directory
 
-const parse = require('csv-parse/lib/sync');
 const fs = require('fs');
-const path = require('path');
-const stringify = require('csv-stringify/lib/sync');
+const csvParse = require('csv-parse/lib/sync');
+const csvStringify = require('csv-stringify/lib/sync');
 const cli = require('../lib/cli-common');
 
 const CSV_PARSE_OPTIONS = { columns: true, skip_empty_lines: true };
+const CSV_STRINGIFY_OPTIONS = { header: true };
 
 const HEADERS = [
   { input: 'DCODE_name', output: 'Division' },
@@ -21,68 +23,45 @@ const HEADERS = [
 ];
 
 function feetToMeters(feet) {
-  return (feet * 0.3048).toFixed(2);
+  return (feet * 0.3048).toFixed(1); // our algorithms do not care about fractions of meters
 }
 
-function getCsvFilepaths(cliArgs) {
-  // if filepaths are not defined, use default
-  if (cliArgs.paths == null) {
-    const filePathList = [];
-    const defaultDir = 'r-data-csv';
-    const files = fs.readdirSync(defaultDir);
-    const csvFiles = files.filter(file => file.endsWith('.csv'));
-    for (const file of csvFiles) {
-      const filePath = path.join(defaultDir, file);
-      filePathList.push(filePath);
-    }
-    return filePathList;
-  } else {
-    if (!cliArgs.paths.every(filepath => filepath.endsWith('.csv'))) {
-      console.error('User defined files include files which are not CSV format');
-    }
-    return cliArgs.paths;
-  }
-}
-
-function getFiles(filepaths) {
+function readFiles(filepaths) {
   const inputCsvFiles = [];
   for (const filepath of filepaths) {
     const rawFile = fs.readFileSync(filepath);
-    const csvFile = parse(rawFile, CSV_PARSE_OPTIONS);
+    const csvFile = csvParse(rawFile, CSV_PARSE_OPTIONS);
     inputCsvFiles.push(csvFile);
   }
-  return inputCsvFiles;
+  return inputCsvFiles.flat();
 }
 
-function consolidateCsv(inputCsvFiles) {
-  let retArr = [];
-  inputCsvFiles.forEach(file => { retArr = retArr.concat(file); });
-  return retArr;
-}
-
-function formatCsv(inputCsv) {
+// convers from the input format from rdata into our desired output:
+// 1. extracts only the columns we care about (see HEADERS)
+// 2. converts depth from feet to meters
+function convert(inputCsv) {
   const outputCsv = [];
   for (const inputRecord of inputCsv) {
     const outputRecord = {};
-    HEADERS.forEach(header => { outputRecord[header.output] = inputRecord[header.input]; });
+
+    for (const header of HEADERS) {
+      outputRecord[header.output] = inputRecord[header.input];
+    }
+
+    // convert depth to meters
     outputRecord.Depth = feetToMeters(outputRecord.Depth);
+
     outputCsv.push(outputRecord);
   }
   return outputCsv;
 }
 
-function convertToCsv(outputJson) {
-  const outputHeaders = HEADERS.map(header => header.output);
-  const outputHeadersRecord = outputHeaders.join(',') + '\n';
-  return outputHeadersRecord + stringify(outputJson);
-}
-
 function main(cliArgs) {
-  const csvFilepaths = getCsvFilepaths(cliArgs); // get filepaths from directory (csv only)
-  const inputCsvFiles = getFiles(csvFilepaths); // get csv files from filepaths
-  const allInputCsvFiles = consolidateCsv(inputCsvFiles); // join all csv files into one array
-  const outputJson = formatCsv(allInputCsvFiles); // format csv files
-  const outputCsv = convertToCsv(outputJson); // convert from json format to csv
+  const inputCsvData = readFiles(cliArgs.paths);
+  const outputData = convert(inputCsvData);
+  const outputCsv = csvStringify(outputData, CSV_STRINGIFY_OPTIONS);
+
+  // send to stdout
   console.log(outputCsv);
 }
 
