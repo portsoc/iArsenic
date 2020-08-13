15cea854fedd79fd13f68eedae8a435d78fdc6d6
Jacek Kopecky <jacek@jacek.cz>
2020-08-13 18:00:45 +0100

    more work
    Co-Authored-By: N. D. Klasson <TxJson@users.noreply.github.com>
    Co-Authored-By: null <JavaRip@users.noreply.github.com>

--- preprocessing/data-import/find-name-corrections.js
+++ preprocessing/data-import/find-name-corrections.js
@@ -2,133 +2,94 @@ const csvLoader = require('../lib/load-data');
 const cli = require('../lib/cli-common');
 const prompt = require('prompt-sync')();
 
-const REGIONS = { div: 1, dis: 2, upa: 3, uni: 4 };
-
-function getCorrections(corrections, correctNameData, uncheckedNameData, ...regionArr) {
-  const misspeltRegion = regionArr[regionArr.length - 1].name;
-
+function getCorrections(corrections, correctNameData, uncheckedNameData, region) {
   // only shows regions inside misspelt region's parent region
   // corrections to parent region names are made on the fly
-  const regionLevelArr = getRegions(correctNameData, regionArr);
+  const correctlySpelledSiblings = getCorrectlySpelledSiblingRegions(correctNameData, region);
 
-  const userInput = getUserInput(regionArr, regionLevelArr, misspeltRegion);
+  const chosenSibling = chooseCorrectSibling(correctlySpelledSiblings, region);
 
-  if (userInput === '') return; // if user enters space skip correction
+  if (chosenSibling == null) return false; // if the user doesn't select a sibling, move on
 
-  const correctSpelling = regionLevelArr[userInput].name;
-  const correction = { correct: correctSpelling, incorrect: misspeltRegion };
+  const correctSpelling = chosenSibling.name;
+  const correction = { correct: correctSpelling, incorrect: region.name }; // TODO fill in the path to the region
   corrections.push(correction); // TODO write corrections to file (and possible apply to csv??)
-  applyCorrection(correction, uncheckedNameData, regionArr);
+
+  region.name = correctSpelling;
+  return true;
 }
 
-function applyCorrection(correction, uncheckedNameData, regionArr) {
-  const regionDepth = regionArr.length;
-  for (const div of Object.values(uncheckedNameData)) {
-    if (regionDepth === REGIONS.div && correction.incorrect === div.name) {
-      duplicateDelete(correction, div); // apply correction (duplicate and delete method)
-    }
-    for (const dis of Object.values(div.districts)) {
-      // TODO put big if statement into function
-      if (regionDepth === REGIONS.dis && correction.incorrect === dis.name && dis.parentRegion === div) {
-        duplicateDelete(correction, dis);
-      }
-      for (const upa of Object.values(dis.upazilas)) {
-        if (regionDepth === REGIONS.upa && correction.incorrect === upa.name && upa.parentRegion === dis) {
-          duplicateDelete(correction, upa);
-        }
-        for (const uni of Object.values(upa.unions)) {
-          if (regionDepth === REGIONS.uni && correction.incorrect === uni.name && upa.parentRegion === upa) {
-            duplicateDelete(correction, uni);
-          }
-        }
-      }
-    }
-  }
-}
-
-function duplicateDelete(correction, region) {
-  // go to region parent
-  const parentRegion = region.parentRegion;
-  // duplicate parent[region.incorrectName] to parent[region.correctName]
-  parentRegion[correction.correct] = region;
-  // correct parent[correctName].name to correctName
-  parentRegion[correction.correct].name = correction.correct;
-  // delete parent[region.incorrectName]
-  const regionLevelKey = getLevelKey(region);
-
-  // if null regionLevel is district so no subkey required
-  if (regionLevelKey != null) {
-    delete parentRegion[regionLevelKey][correction.incorrect];
-  } else {
-    delete parentRegion[correction.incorrect];
-  }
-}
-
-function getLevelKey(region) {
-  if (region.districts != null) return null;
-  if (region.upazilas != null) return 'districts';
-  if (region.unions != null) return 'upazilas';
-  return 'unions';
-}
-
-function getUserInput(regionArr, regionLevelArr, misspeltRegion) {
-  let inputIsValid = false;
-  let userInput;
-  while (!inputIsValid) {
+function chooseCorrectSibling(correctSiblings, misspeltRegion) {
+  while (true) {
     console.log('');
-    for (let i = 0; i < regionLevelArr.length; i += 1) {
-      console.log(i, regionLevelArr[i].name);
-      // TODO also display child regions (order by alphabetically)
+    for (let i = 0; i < correctSiblings.length; i += 1) {
+      const sibling = correctSiblings[i];
+      console.log(i + 1, sibling.name, getSubregionNames(sibling));
     }
 
-    console.log('Incorrect region: ', misspeltRegion);
-    userInput = prompt('Input ID of correct spelling: ');
+    console.log('Incorrect region: ', misspeltRegion.name, getSubregionNames(misspeltRegion));
+
+    const userInput = prompt('Input ID of correct spelling: ');
+
+    // quit on ctrl-c
+    if (userInput == null) process.exit(0);
 
     // if user presses enter skip this correction
-    if (userInput === '') break;
+    if (userInput === '') return null;
 
-    inputIsValid = validInput(userInput, regionLevelArr.length);
-    if (!inputIsValid) console.log('INVALID INPUT');
+    const inputIsValid = validInput(userInput, correctSiblings.length);
+    if (inputIsValid) {
+      return correctSiblings[userInput - 1];
+    }
+
+    // input wasn't valid
+    console.log(`INVALID INPUT, please enter a number up to ${correctSiblings.length}`);
   }
-  return userInput;
+}
+
+function getSubregionNames(region) {
+  if (region.subRegionsArr == null) return '';
+
+  const namesArr = region.subRegionsArr.map(r => r.name);
+  return `(${namesArr.sort().join(', ')})`;
 }
 
 function validInput(input, regionArrLength) {
-  if (input == null) process.exit(0);
   if (input >= regionArrLength || input < 0) return false; // if answer out of bounds of array
   if (isNaN(input)) return false; // if answer is not a number return false
   return true;
 }
 
-function getRegions(correctNameData, regionArr) {
-  const regionDepth = regionArr.length;
-  let regionDepthArr = [];
+function getCorrectlySpelledSiblingRegions(correctNameData, region) {
+  const parentNameArr = findAncestorRegionNames(region);
+  const correctlyNamedParent = findRegionByNameArr(correctNameData, parentNameArr);
+  return correctlyNamedParent.subRegionsArr;
+}
 
-  for (const div of Object.values(correctNameData)) {
-    if (regionDepth === REGIONS.div) {
-      regionDepthArr.push(div);
-    }
-    if (regionArr[1] == null) continue;
-    for (const dis of Object.values(div.districts)) {
-      if (regionDepth === REGIONS.dis && regionArr[0].name === div.name) {
-        regionDepthArr.push(dis);
-      }
-      if (regionArr[2] == null) continue;
-      for (const upa of Object.values(dis.upazilas)) {
-        if (regionDepth === REGIONS.upa && regionArr[1].name === dis.name) {
-          regionDepthArr.push(upa);
-        }
-        if (regionArr[3] == null) continue;
-        for (const uni of Object.values(upa.unions)) {
-          if (regionDepth === REGIONS.uni && regionArr[2].name === upa.name) {
-            regionDepthArr.push(uni);
-          }
-        }
-      }
-    }
+function findAncestorRegionNames(region) {
+  let currentRegion = region.parentRegion;
+  const names = [];
+
+  while (currentRegion != null) {
+    names.unshift(currentRegion.name);
+    currentRegion = currentRegion.parentRegion;
   }
-  regionDepthArr = regionDepthArr.sort((a, b) => a.name.localeCompare(b.name));
-  return regionDepthArr;
+
+  return names;
+}
+
+function findRegionByNameArr(dataset, arr) {
+  const div = dataset[arr[0]];
+  if (arr.length === 1) return div;
+
+  const dis = div.districts[arr[1]];
+  if (arr.length === 2) return dis;
+
+  const upa = dis.upazilas[arr[2]];
+  if (arr.length === 3) return upa;
+
+  const uni = upa.unions[arr[3]];
+  return uni;
 }
 
 function correctDataContains(correctNameData, ...regionNames) {
@@ -143,21 +104,24 @@ function correctDataContains(correctNameData, ...regionNames) {
   if (restOfNames.length === 0) return true;
 
   // check for the deeper names in the subregions
-  return correctDataContains(topRegion.subRegions, restOfNames);
+  return correctDataContains(topRegion.subRegions, ...restOfNames);
 }
 
 function addRelativeRegionLinks(dataset) {
   for (const div of Object.values(dataset)) {
     div.siblingRegions = dataset;
     div.subRegions = div.districts;
+    div.subRegionsArr = Object.values(div.subRegions);
     for (const dis of Object.values(div.districts)) {
       dis.parentRegion = div;
       dis.siblingRegions = div.districts;
       dis.subRegions = dis.upazilas;
+      dis.subRegionsArr = Object.values(dis.subRegions);
       for (const upa of Object.values(dis.upazilas)) {
         upa.parentRegion = dis;
         upa.siblingRegions = dis.upazilas;
         upa.subRegions = upa.unions;
+        upa.subRegionsArr = Object.values(upa.subRegions);
         for (const uni of Object.values(upa.unions)) {
           uni.parentRegion = upa;
           uni.siblingRegions = upa.unions;
@@ -179,19 +143,22 @@ function main(cliArgs) {
 
   for (const div of Object.values(uncheckedNameData)) {
     if (!correctDataContains(correctNameData, div.name)) {
+      // TODO continue if getCorrections returns false
       getCorrections(corrections, correctNameData, uncheckedNameData, div);
     }
     for (const dis of Object.values(div.districts)) {
       if (!correctDataContains(correctNameData, div.name, dis.name)) {
-        getCorrections(corrections, correctNameData, uncheckedNameData, div, dis);
+        // TODO continue if getCorrections returns false
+        getCorrections(corrections, correctNameData, uncheckedNameData, dis);
       }
       for (const upa of Object.values(dis.upazilas)) {
         if (!correctDataContains(correctNameData, div.name, dis.name, upa.name)) {
-          getCorrections(corrections, correctNameData, uncheckedNameData, div, dis, upa);
+          // TODO continue if getCorrections returns false
+          getCorrections(corrections, correctNameData, uncheckedNameData, upa);
         }
         for (const uni of Object.values(upa.unions)) {
           if (!correctDataContains(correctNameData, div.name, dis.name, upa.name, uni.name)) {
-            getCorrections(corrections, correctNameData, uncheckedNameData, div, dis, upa, uni);
+            getCorrections(corrections, correctNameData, uncheckedNameData, uni);
           }
         }
       }
