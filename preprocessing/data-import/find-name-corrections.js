3397f1ef23e604dad9b922d041c7135a1e74f3e2
Jacek Kopecky <jacek@jacek.cz>
2020-08-17 15:58:16 +0100

    review
    Co-Authored-By: N. D. Klasson <TxJson@users.noreply.github.com>
    Co-Authored-By: null <JavaRip@users.noreply.github.com>

--- preprocessing/data-import/find-name-corrections.js
+++ preprocessing/data-import/find-name-corrections.js
@@ -11,7 +11,12 @@ const CSV_PARSE_OPTIONS = {
   skip_empty_lines: true,
 };
 
-function getCorrections(correctNameData, uncheckedNameData, region) {
+function correct(correctNameData, uncheckedNameData, region) {
+  if (correctDataContains(correctNameData, ...findAncestorRegionNames(region), region.name)) {
+    // it's already correct
+    return true;
+  }
+
   // only shows regions inside misspelt region's parent region
   // corrections to parent region names are made on the fly
   const correctlySpelledSiblings = getCorrectlySpelledSiblingRegions(correctNameData, region);
@@ -27,39 +32,36 @@ function getCorrections(correctNameData, uncheckedNameData, region) {
     incorrect: region.name,
   };
 
+  // correct the name in the data
+  region.name = correctSpelling;
+
+  // save the correction
   appendCorrectionToFile(correction);
 
-  region.name = correctSpelling;
   return true;
 }
 
-async function appendCorrectionToFile(correction) {
-  let correctionRecord =
+function appendCorrectionToFile(correction) {
+  const correctionRecord =
     correction.path.join('#') + ',' +
     correction.correct + ',' +
     correction.incorrect + '\n';
-  const correctionFile = path.join(__dirname, '/corrections.csv');
-  if (!fs.existsSync(correctionFile)) {
-    const headers = 'path,correct,incorrect' + '\n';
-    correctionRecord = headers + correctionRecord;
-  }
-  await fs.appendFileSync(correctionFile, correctionRecord); // TODO log error & get path from -o
+  const correctionFile = path.join(__dirname, 'corrections.csv');
+  fs.appendFileSync(correctionFile, correctionRecord); // TODO log error & get path from -o
 }
 
 function chooseCorrectSibling(correctSiblings, misspeltRegion) {
   while (true) {
     console.log('');
-    let subRegionNames;
     for (let i = 0; i < correctSiblings.length; i += 1) {
       const sibling = correctSiblings[i];
       const siblingName = colors.underline(sibling.name);
-      subRegionNames = colors.italic(getSubregionNames(sibling));
+      const subRegionNames = colors.italic(getSubregionNames(sibling));
       console.log(i + 1, siblingName, subRegionNames);
-      console.log('---------------');
     }
 
     const misspeltRegionName = colors.bold(misspeltRegion.name);
-    subRegionNames = colors.italic(getSubregionNames(misspeltRegion));
+    const subRegionNames = colors.italic(getSubregionNames(misspeltRegion));
     console.log('Incorrect region: ', misspeltRegionName, subRegionNames);
 
     const userInput = prompt('Input ID of correct spelling: ');
@@ -76,7 +78,7 @@ function chooseCorrectSibling(correctSiblings, misspeltRegion) {
     }
 
     // input wasn't valid
-    console.log(colors.red.bold(`\nINVALID INPUT, please enter a number up to ${correctSiblings.length}`));
+    console.log(colors.red.bold(`\nINVALID INPUT, please enter a number 1-${correctSiblings.length}`));
   }
 }
 
@@ -116,7 +118,7 @@ function findAncestorRegionNames(region) {
 
 function findRegionByNameArr(dataset, arr) {
   const div = dataset[arr[0]];
-  if (arr.length === 1) return div; // FIXME array length can be 0 when region is division
+  if (arr.length === 1) return div;
 
   const dis = div.districts[arr[1]];
   if (arr.length === 2) return dis;
@@ -144,50 +146,37 @@ function correctDataContains(correctNameData, ...regionNames) {
 }
 
 function applyCorrections(dataset, corrections) {
-  // if corrections is null or undefined; return
-  // then if defined but length is not > 0; return
-  if (!corrections?.length) return;
-
-  let regionToCorrect;
   for (const correction of corrections) {
     const pathArr = correction.path.split('#');
     if (pathArr[0] !== '') {
-      regionToCorrect = dataset[pathArr[0]].subRegions;
+      let regionToCorrect = dataset[pathArr[0]].subRegions;
       pathArr.shift();
       for (const objectPath of pathArr) {
         regionToCorrect = regionToCorrect[objectPath].subRegions;
       }
       regionToCorrect = regionToCorrect[correction.incorrect];
-      regionToCorrect.parentRegion[correction.correct] = regionToCorrect;
       regionToCorrect.parentRegion[correction.correct].name = correction.correct;
-      delete regionToCorrect.parentRegion[correction.incorrect];
     } else {
-      regionToCorrect = dataset[correction.incorrect];
+      const regionToCorrect = dataset[correction.incorrect];
       regionToCorrect.name = correction.correct;
-      dataset[correction.correct] = regionToCorrect;
-      delete dataset[correction.incorrect];
     }
   }
 }
 
 function addRelativeRegionLinks(dataset) {
   for (const div of Object.values(dataset)) {
-    div.siblingRegions = dataset;
     div.subRegions = div.districts;
     div.subRegionsArr = Object.values(div.subRegions);
     for (const dis of Object.values(div.districts)) {
       dis.parentRegion = div;
-      dis.siblingRegions = div.districts;
       dis.subRegions = dis.upazilas;
       dis.subRegionsArr = Object.values(dis.subRegions);
       for (const upa of Object.values(dis.upazilas)) {
         upa.parentRegion = dis;
-        upa.siblingRegions = dis.upazilas;
         upa.subRegions = upa.unions;
         upa.subRegionsArr = Object.values(upa.subRegions);
         for (const uni of Object.values(upa.unions)) {
           uni.parentRegion = upa;
-          uni.siblingRegions = upa.unions;
         }
       }
     }
@@ -197,44 +186,40 @@ function addRelativeRegionLinks(dataset) {
 function main(cliArgs) {
   const correctNameData = csvLoader(cliArgs.paths); // use -p flag
 
-  // temporary use of path so the script can be run outside from the data-import directory
-  const testFile = path.join(__dirname, '/small-test-dataset.csv');
-  const uncheckedNameData = csvLoader([testFile]); // use -i flag
+  // TODO temporary default input file before we support -i
+  const testFile = path.join(__dirname, 'small-test-dataset.csv');
+  const uncheckedNameData = csvLoader([testFile]);
 
   addRelativeRegionLinks(correctNameData);
   addRelativeRegionLinks(uncheckedNameData);
 
-
-  // TODO get corrections filepath, probably from -o
-  // temporary use of path so the script can be run outside from the data-import directory
+  // TODO temporary default output file before we support -o
   const correctionFile = path.join(__dirname, '/corrections.csv');
   let corrections;
-  if (fs.existsSync(correctionFile)) {
+  if (!fs.existsSync(correctionFile)) {
+    const headers = 'path,correct,incorrect' + '\n';
+    fs.appendFileSync(correctionFile, headers);
+    corrections = [];
+  } else {
     corrections = parse(fs.readFileSync(correctionFile), CSV_PARSE_OPTIONS);
   }
 
   applyCorrections(uncheckedNameData, corrections);
 
   for (const div of Object.values(uncheckedNameData)) {
-    let correctionState = false;
-    if (!correctDataContains(correctNameData, div.name)) {
-      correctionState = getCorrections(correctNameData, uncheckedNameData, div);
-      if (!correctionState) continue;
-    }
+    const corrected = correct(correctNameData, uncheckedNameData, div);
+    if (!corrected) continue; // don't try to correct sub-region names
+
     for (const dis of Object.values(div.districts)) {
-      if (!correctDataContains(correctNameData, div.name, dis.name)) {
-        correctionState = getCorrections(correctNameData, uncheckedNameData, dis);
-        if (!correctionState) continue;
-      }
+      const corrected = correct(correctNameData, uncheckedNameData, dis);
+      if (!corrected) continue; // don't try to correct sub-region names
+
       for (const upa of Object.values(dis.upazilas)) {
-        if (!correctDataContains(correctNameData, div.name, dis.name, upa.name)) {
-          correctionState = getCorrections(correctNameData, uncheckedNameData, upa);
-          if (!correctionState) continue;
-        }
+        const corrected = correct(correctNameData, uncheckedNameData, upa);
+        if (!corrected) continue; // don't try to correct sub-region names
+
         for (const uni of Object.values(upa.unions)) {
-          if (!correctDataContains(correctNameData, div.name, dis.name, upa.name, uni.name)) {
-            getCorrections(correctNameData, uncheckedNameData, uni);
-          }
+          correct(correctNameData, uncheckedNameData, uni);
         }
       }
     }
