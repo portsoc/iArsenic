171aa7c6121160185e54f70453f448c24bc05e3b
Jacek Kopecky <jacek@jacek.cz>
2021-04-28 14:29:36 +0100

    :pencil: clarifications

--- preprocessing/lib/name-corrections.js
+++ preprocessing/lib/name-corrections.js
@@ -1,6 +1,7 @@
 /*
- * List of name corrections for division/district/upazila/union spelling variations.
- * Loads the name corrections
+ * This file handles name corrections for division/district/upazila/union/mouza
+ * spelling variations. Name corrections are builtin (below) and loaded from
+ * files in /data/name-corrections
  */
 
 const corrections = new Map();
