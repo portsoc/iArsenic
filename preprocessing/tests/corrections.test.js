a285f84efd19f1904d46c143ac38f674c62c8731
Jacek Kopecky <jacek@jacek.cz>
2021-07-27 13:21:24 +0100

    changes from review
    Co-Authored-By: Benjamin Kyd <benjaminkyd@gmail.com>

--- preprocessing/tests/corrections.test.js
+++ preprocessing/tests/corrections.test.js
@@ -6,46 +6,43 @@ const corrections = require('../lib/name-corrections');
 const { readTheCSVFiles } = require('../lib/load-data');
 
 describe('corrections', () => {
-  // let's say corrections is like this:
-  // 'Sylhet#Sunamganj#Dharampasha#Dakshin  Sukhairrajapur':
-  //   ['Sylhet', 'Sunamganj', 'Dharampasha', 'Dakshin Sukhairrajapur'],
-  // 'Rajshahi#Dinajpur':
-  //   ['Rangpur', 'Dinajpur']
-
-  // firstly we should load name corrections from the test
-  const correctionsData = readTheCSVFiles(path.join(__dirname, 'test-corrections.csv'));
+  // load testing name corrections
+  const correctionsData = readTheCSVFiles(path.join(__dirname, 'corrections.test.csv'));
   corrections.loadCorrections(correctionsData);
 
-  // if we don't have a correct, the array is assumed to be correct
-  it('should assume array to be correct if we don\'t have a correct', () => {
+  it('should assume array to be correct if we don\'t have a correction', () => {
     expect(corrections.correct(['a', 'b'])).toEqual(['a', 'b']);
+
+    expect(corrections.correct(['match', 'it']))
+      .toEqual(['match', 'it']);
   });
 
-  // if we have a strictly matching correction, we apply it
-  it('should apply a correction if we have a strictly matching correction', () => {
-    expect(corrections.correct(['division', 'district', 'upazilla', 'union', 'mouza']))
-      .toEqual(['division', 'district', 'upazilla', 'union', 'misspelling']);
+  it('should correct if we have a strictly matching correction', () => {
+    expect(corrections.correct(['division', 'district', 'upazilla', 'union', 'misspelling']))
+      .toEqual(['division', 'district', 'upazilla', 'union', 'mouza']);
 
     expect(corrections.correct(['Rajshahi', 'Dinajpur']))
       .toEqual(['Rangpur', 'Dinajpur']);
+
+    expect(corrections.correct(['Rajshahi', 'Dinajpur', 'somewhere']))
+      .toEqual(['actually', 'somewhere', 'else']);
 
     expect(corrections.correct(['strictly', 'matching']))
       .toEqual(['strict', 'matching']);
 
     // and with mouzas
-    expect(corrections.correct(['match', 'it', 'strictly', 'with', 'mouzas']))
-      .toEqual(['mach', 'it', 'strictly', 'with', 'mouzas']);
+    expect(corrections.correct(['mach', 'it', 'strictly', 'with', 'mouzas']))
+      .toEqual(['match', 'it', 'strictly', 'with', 'mouzas']);
   });
 
-  // if we have a partial correction, we apply it
-  it('should apply a correction if there is a partial correction', () => {
-    expect(corrections.correct(['match', 'it']))
-      .toEqual(['match', 'it']);
-
+  it('should apply parial corrections when no strict correction available', () => {
     expect(corrections.correct(['Rajshahi', 'Dinajpur', 'a']))
       .toEqual(['Rangpur', 'Dinajpur', 'a']);
 
-    expect(corrections.correct(['Rajshahi', 'Dinajpur', 'a', 'b']))
-      .toEqual(['Rangpur', 'Dinajpur', 'a', 'b']);
+    expect(corrections.correct(['Rajshahi', 'Dinajpur', 'a', 'b', 'c']))
+      .toEqual(['Rangpur', 'Dinajpur', 'a', 'b', 'c']);
+
+    expect(corrections.correct(['Rajshahi', 'Dinajpur', 'somewhere', 'foo']))
+      .toEqual(['actually', 'somewhere', 'else', 'foo']);
   });
 });
