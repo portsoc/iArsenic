4894d55ceb63cca72aea5b19154dbda2a42fffe8
Jacek Kopecky <jacek@jacek.cz>
2021-07-27 14:12:27 +0100

    add flooding stratum
    also make stratum.header optional and move 65-90 stratum before 0-90
    
    Co-Authored-By: Benjamin Kyd <benjaminkyd@gmail.com>

--- preprocessing/cli/data-per-strata.js
+++ preprocessing/cli/data-per-strata.js
@@ -5,13 +5,14 @@ const model5 = require('../models/model5-preprocessor.js');
 const STRATA = [
   { min: 0, max: Infinity, header: 'depth 0+' },
   { min: 0, max: 15.3, header: 'depth 0-15.3', strataKey: 's15' },
+  { min: 0, max: 15.3, header: null /* same count as above */, strataKey: 's15Flood' },
   { min: 15.3, max: 45, header: 'depth 15.3-45', strataKey: 's45' },
   { min: 45, max: 65, header: 'depth 45-65', strataKey: 's65' },
+  { min: 65, max: 90, header: 'depth 65-90', strataKey: 's90' },
   { min: 0, max: 90, header: 'depth 0-90' },
   { min: 90, max: 150, header: 'depth 90-150', strataKey: 's150' },
   { min: 150, max: Infinity, header: 'depth 150+', strataKey: 'sD' },
   { min: 90, max: Infinity, header: 'depth 90+' },
-  { min: 65, max: 90, header: 'depth 65-90', strataKey: 's90' },
 ];
 
 function main(options) {
@@ -32,7 +33,7 @@ function main(options) {
 
   // Adds all the stratum headers to the headers array
   for (const stratum of STRATA) {
-    headers.push(stratum.header);
+    if (stratum.header) headers.push(stratum.header);
     if (stratum.strataKey != null) {
       headers.push('distKm');
       headers.push('count');
@@ -88,11 +89,11 @@ function pushRecord(records, div, dis, upa, uni, mou, wellCountObj) {
   const record = [div, dis, upa, uni, mou];
 
   for (const stratum of STRATA) {
-    record.push(wellCountObj[stratum.header]);
+    if (stratum.header) record.push(wellCountObj[stratum.header]);
 
     // add information about model 5 widening
-    const wideningKey = stratum.strataKey + 'WideningRequired';
     if (stratum.strataKey != null) {
+      const wideningKey = stratum.strataKey + 'WideningRequired';
       const widening = wellCountObj[wideningKey];
       if (uni === '###') {
         record.push('', ''); // we don't aggregate widening information
@@ -110,13 +111,13 @@ function pushRecord(records, div, dis, upa, uni, mou, wellCountObj) {
 
 function initStratas(obj) {
   for (const stratum of STRATA) {
-    obj[stratum.header] = 0;
+    if (stratum.header) obj[stratum.header] = 0;
   }
 }
 
 function countStratas(regions, well) {
   for (const stratum of STRATA) {
-    if (well.depth >= stratum.min && well.depth < stratum.max) {
+    if (stratum.header && well.depth >= stratum.min && well.depth < stratum.max) {
       for (const region of regions) {
         region[stratum.header] += 1;
       }
