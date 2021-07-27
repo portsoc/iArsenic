d7fc02c39b6927071ec4819409c202d01c132597
Ben <benjaminkyd@gmail.com>
2021-07-27 13:08:16 +0100

    re-run produce-aggregate-data with fixes

--- docs/estimator.js
+++ docs/estimator.js
@@ -1,5 +1,5 @@
 // model: model5
-// generated: Wed Jul 14 2021 16:30:49 GMT+0100 (British Summer Time)
+// generated: Mon Jul 26 2021 23:54:51 GMT+0100 (British Summer Time)
 // input data: default
 function round(x, magnitude, dir = 1) {
   if (x % magnitude === 0) {
