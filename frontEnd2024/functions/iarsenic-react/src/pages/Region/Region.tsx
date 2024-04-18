e7b7e22afc2b0216967e591bc42a382d2ac1df64
JavaRip <up941226@myport.ac.uk>
2024-04-18 10:55:21 +0100

    add form validation to region

--- frontEnd2024/functions/iarsenic-react/src/pages/Region/Region.tsx
+++ frontEnd2024/functions/iarsenic-react/src/pages/Region/Region.tsx
@@ -11,20 +11,40 @@ export default function Region(): JSX.Element {
     const [selectedUpazila, setSelectedUpazila] = useState<DropdownUpazila | null>(null);
     const [selectedUnion, setSelectedUnion] = useState<DropdownUnion | null>(null);
     const [selectedMouza, setSelectedMouza] = useState<string | null>(null);
-
-    async function fetchDropdownData() {
-        const response = await fetch(`${config.basePath}/model5/dropdown-data.json`);
-        const data: DropdownDivision[] = await response.json();
-        setDropdownData(data);
-    }
-
-    useEffect(() => {
-        fetchDropdownData();
-    }, []);
+    const [errors, setErrors] = useState({
+        division: false,
+        district: false,
+        upazila: false,
+        union: false,
+        mouza: false
+    });
 
     const districtOptions = selectedDivision?.districts || [];
     const upazilaOptions = selectedDistrict?.upazilas || [];
     const unionOptions = selectedUpazila?.unions || [];
+
+    async function fetchDropdownData() {
+        const response = await fetch(`${config.basePath}/model5/dropdown-data.json`);
+        const data = await response.json();
+        setDropdownData(data);
+    }
+
+    function handleValidation() {
+        const newErrors = {
+            division: !selectedDivision,
+            district: !selectedDistrict,
+            upazila: !selectedUpazila,
+            union: !selectedUnion,
+            mouza: !selectedMouza
+        };
+        setErrors(newErrors);
+
+        return !Object.values(newErrors).some(Boolean);
+    };
+
+    useEffect(() => {
+        fetchDropdownData();
+    }, []);
 
     return (
         <>
@@ -39,10 +59,19 @@ export default function Region(): JSX.Element {
                         setSelectedDistrict(null);
                         setSelectedUpazila(null);
                         setSelectedUnion(null);
+                        setErrors(e => ({ ...e, division: false }));
                     }}
                     getOptionLabel={(option) => option.division}
-                    renderInput={(params) => <TextField {...params} label="Division" />}
+                    renderInput={(params) => {
+                        return (
+                            <TextField {...params} label="Division"
+                                error={errors.division}
+                                helperText={errors.division ? 'Please select a division' : ''}
+                            />
+                        )
+                    }}
                 />
+
                 <Autocomplete
                     options={districtOptions}
                     value={selectedDistrict}
@@ -50,41 +79,76 @@ export default function Region(): JSX.Element {
                         setSelectedDistrict(newValue);
                         setSelectedUpazila(null);
                         setSelectedUnion(null);
+                        setErrors(e => ({ ...e, district: false }));
                     }}
                     getOptionLabel={(option) => option.district}
-                    renderInput={(params) => <TextField {...params} label="District" />}
-                    disabled={!selectedDivision}
+                    renderInput={(params) => {
+                        return (
+                            <TextField {...params} label="District"
+                                error={errors.district}
+                                helperText={errors.district ? 'Please select a district' : ''}
+                                disabled={!selectedDivision}
+                            />
+                        )
+                    }}
                 />
+
                 <Autocomplete
                     options={upazilaOptions}
                     value={selectedUpazila}
                     onChange={(_, newValue) => {
                         setSelectedUpazila(newValue);
                         setSelectedUnion(null);
+                        setErrors(e => ({ ...e, upazila: false }));
                     }}
                     getOptionLabel={(option) => option.upazila}
-                    renderInput={(params) => <TextField {...params} label="Upazila" />}
-                    disabled={!selectedDistrict}
+                    renderInput={(params) => {
+                        return (
+                            <TextField {...params} label="Upazila"
+                                error={errors.upazila}
+                                helperText={errors.upazila ? 'Please select an upazila' : ''}
+                                disabled={!selectedDistrict}
+                            />
+                        )
+                    }}
                 />
+
                 <Autocomplete
                     options={unionOptions}
                     value={selectedUnion}
                     onChange={(_, newValue) => {
                         setSelectedUnion(newValue);
+                        setErrors(e => ({ ...e, union: false }));
                     }}
                     getOptionLabel={(option) => option.union}
-                    renderInput={(params) => <TextField {...params} label="Union" />}
-                    disabled={!selectedUpazila}
+                    renderInput={(params) => {
+                    return (
+                            <TextField {...params} label="Union"
+                                error={errors.union}
+                                helperText={errors.union ? 'Please select a union' : ''}
+                                disabled={!selectedUpazila}
+                            />
+                        )
+                    }}
                 />
+
                 <Autocomplete
                     options={selectedUnion ? selectedUnion.mouzas : []}
                     value={selectedMouza}
                     onChange={(_, newValue) => {
                         setSelectedMouza(newValue);
+                        setErrors(e => ({ ...e, mouza: false }));
                     }}
                     getOptionLabel={(mouza) => mouza}
-                    renderInput={(params) => <TextField {...params} label="Mouza" />}
-                    disabled={!selectedUnion}
+                    renderInput={(params) => {
+                        return (
+                            <TextField {...params} label="Mouza"
+                                error={errors.mouza}
+                                helperText={errors.mouza ? 'Please select a mouza' : ''}
+                                disabled={!selectedUnion}
+                            />
+                        )
+                    }}
                 />
             </Stack>
 
@@ -92,6 +156,8 @@ export default function Region(): JSX.Element {
                 sx={{ width: '90%', height: '4rem' }}
                 variant='contained'
                 onClick={() => {
+                    if (!handleValidation()) return
+
                     localStorage.setItem('region', JSON.stringify({
                         division: selectedDivision?.division,
                         district: selectedDistrict?.district,
@@ -99,7 +165,7 @@ export default function Region(): JSX.Element {
                         union: selectedUnion?.union,
                         mouza: selectedMouza
                     }));
-                    navigate(`${config.basePath}/staining`)
+                    navigate(`${config.basePath}/staining`);
                 }}
             >
                 Next Step
