88e1bee8aefaaaa42cf658b68f20c8ab8eb18ebf
JavaRip <up941226@myport.ac.uk>
2024-06-16 20:26:15 +0100

    update vite config

--- frontEnd2024/functions/iarsenic-react/vite.config.ts
+++ frontEnd2024/functions/iarsenic-react/vite.config.ts
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [react()],
     base: '/',
-    build: {
-        rollupOptions: {
-            external: ['model5/estimator.js']
-        }
-    }
 });
