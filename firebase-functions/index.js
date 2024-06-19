fa11ad559e5383bbac5049b3c406a9170d148641
JavaRip <up941226@myport.ac.uk>
2024-06-19 19:21:52 +0100

    add route to save prediction data

--- firebase-functions/index.js
+++ firebase-functions/index.js
@@ -2,6 +2,11 @@ import functions from 'firebase-functions';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
+
+initializeApp();
+const db = getFirestore();
 
 const app = express();
 app.use(cors());
@@ -14,6 +19,47 @@ app.get('/api/healthcheck', (_, res) => {
         status: 'ok',
         message: `Server is running. Timestamp: ${new Date().toISOString()}`,
     });
+});
+
+app.post('/api/save-prediction', async (req, res) => {
+    const { data } = req.body;
+
+    try {
+        const predictionsRef = db.collection('default');
+
+        await predictionsRef.add({
+            predictors: {
+                regionKey: {
+                    division: data.predictors.regionKey.division,
+                    district: data.predictors.regionKey.district,
+                    upazila: data.predictors.regionKey.upazila,
+                    union: data.predictors.regionKey.union,
+                    mouza: data.predictors.regionKey.mouza,
+                },
+                depth: {
+                    unit: data.predictors.depth.unit,
+                    value: data.predictors.depth.value,
+                },
+                flooding: data.predictors.flooding,
+                wellStaining: data.predictors.wellStaining,
+                utensilStaining: data.predictors.utensilStaining,
+            },
+            createdAt: Timestamp.now(),
+            model: data.model,
+            prediction: data.prediction,
+            requesterIp: req.ip,
+            language: data.language,
+            location: {
+                latitude: data.location.latitude,
+                longitude: data.location.longitude,
+            },
+        });
+
+        res.json({ status: 'ok' });
+    } catch (error) {
+        console.error('Error saving prediction:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.get('*', (_, res) => {
