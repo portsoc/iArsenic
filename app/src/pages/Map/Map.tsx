5e6d319182b28c27f9a22e3e95109811cc45e8f4
JavaRip <up941226@myport.ac.uk>
2024-07-09 20:32:36 +0100

    put upa map into separate file

--- app/src/pages/Map/Map.tsx
+++ app/src/pages/Map/Map.tsx
@@ -1,26 +1,25 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './map.css';
-import { CircularProgress, Stack, Typography } from '@mui/material';
+import { CircularProgress, Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { LatLngExpression } from 'leaflet';
+import { LatLngExpression, GeoJSON } from 'leaflet';
 import config from '../../config';
 import { SessionData } from '../../types';
-import ReactDOMServer from 'react-dom/server';
 import { RegionTranslations } from '../../types';
 import RegionTranslationsFetcher from '../../utils/RegionTranslationsFetcher';
 import Markers from './markers';
+import UpaMap from './upaMap';
 
 export default function Map() {
     const position: LatLngExpression = [23.8041, 90.4152];
-    const [interactiveMap, setInteractiveMap] = useState();
+    const [interactiveMap, setInteractiveMap] = useState<GeoJSON>();
     const [sessionData, setSessionData] = useState<SessionData[]>();
     const [regionTranslations, setRegionTranslations] = useState<RegionTranslations>();
 
     async function getInteractiveMap() {
-        const res = await fetch(`${config.basePath}/api/interactive-map`);
+        const res = await fetch(`${config.basePath}/interactive-map.geojson`);
         const mapData = await res.json();
-        console.log(mapData);
         setInteractiveMap(mapData);
     }
 
@@ -28,79 +27,6 @@ export default function Map() {
         const res = await fetch(`${config.basePath}/api/predictions`);
         const predictions = await res.json();
         setSessionData(predictions.data);
-    }
-
-    // todo put in separate component file
-    function getGeoJSONLayer() {
-        if (!interactiveMap || !regionTranslations) {
-            return (
-                <Stack alignItems='center'>
-                    <CircularProgress />
-                </Stack>
-            );
-        }
-
-        return (
-            <GeoJSON
-                data={interactiveMap}
-                style={(feature) => ({
-                    fillColor: getColor(feature?.properties.as),
-                    weight: 1,
-                    opacity: 1,
-                    color: 'white',
-                    dashArray: '3',
-                    fillOpacity: 0.7,
-                })}
-                onEachFeature={(feature, layer) => { // TODO create feature typing
-                    layer.bindPopup(ReactDOMServer.renderToString(
-                        <>
-                            <Typography className='english' variant='body1'>
-                                Mean As: {Math.floor(feature.properties.as)}
-                            </Typography>
-
-                            <Typography className='bengali' variant='body1'>
-                                BENGALI PLACEHOLDER
-                            </Typography>
-
-                            <Typography className='english' variant='body1'>
-                                Division: {feature.properties.div}
-                            </Typography>
-
-                            <Typography className='bengali' variant='body1'>{`
-                                ${regionTranslations.Divisions.Division}:
-                                ${regionTranslations.Divisions[feature.properties.div]}
-                            `}</Typography>
-
-                            <Typography className='english' variant='body1'>
-                                District: {feature.properties.dis}
-                            </Typography>
-
-                            <Typography className='bengali' variant='body1'>{`
-                                ${regionTranslations.Districts.District}:
-                                ${regionTranslations.Districts[feature.properties.dis]}
-                            `}</Typography>
-
-                            <Typography className='english' variant='body1'>
-                                Upazila: {feature.properties.upa}
-                            </Typography>
-
-                            <Typography className='bengali' variant='body1'>{`
-                                ${regionTranslations.Upazilas.Upazila}:
-                                ${regionTranslations.Upazilas[feature.properties.upa]}
-                            `}</Typography>
-                        </>
-                    ));
-                }}
-            />
-        );
-    }
-
-    // colourise geojson regions based on mean arsenic
-    function getColor(as: number) {
-        return as === null ? 'gray' :
-            as < 10 ? 'rgba(100, 149, 237, 0.5)' :
-            as < 50 ? 'rgba(144, 238, 144, 0.5)' :
-            'rgba(255, 69, 0, 0.5)';
     }
 
     async function getRegionTranslations() {
@@ -127,7 +53,7 @@ export default function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {getGeoJSONLayer()}
+                <UpaMap interactiveMap={interactiveMap} regionTranslations={regionTranslations} />
                 <Markers sessionData={sessionData} regionTranslations={regionTranslations} />
             </MapContainer>
         </Stack>
---
+++ app/src/pages/Map/upaMap.tsx
@@ -0,0 +1,71 @@
+import { Typography } from "@mui/material";
+import ReactDOMServer from "react-dom/server";
+import { GeoJSON } from 'react-leaflet';
+import { RegionTranslations } from "../../types";
+
+type props = {
+    interactiveMap: any,
+    regionTranslations: RegionTranslations,
+}
+
+export default function UpaMap({ interactiveMap, regionTranslations }: props): JSX.Element {
+    function getColor(as: number): string {
+        if (as < 10) return 'rgba(100, 149, 237, 0.5)';
+        if (as < 50) return 'rgba(144, 238, 144, 0.5)';
+        return 'rgba(255, 69, 0, 0.5)';
+    }
+
+    return (
+        <GeoJSON
+            data={interactiveMap}
+            style={(feature) => ({
+                fillColor: getColor(feature?.properties.as),
+                weight: 1,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7,
+            })}
+            onEachFeature={(feature, layer) => { // TODO create feature typing
+                layer.bindPopup(ReactDOMServer.renderToString(
+                    <>
+                        <Typography className='english' variant='body1'>
+                            Mean As: {Math.floor(feature.properties.as)}
+                        </Typography>
+
+                        <Typography className='bengali' variant='body1'>
+                            BENGALI PLACEHOLDER
+                        </Typography>
+
+                        <Typography className='english' variant='body1'>
+                            Division: {feature.properties.div}
+                        </Typography>
+
+                        <Typography className='bengali' variant='body1'>{`
+                            ${regionTranslations.Divisions.Division}:
+                            ${regionTranslations.Divisions[feature.properties.div]}
+                        `}</Typography>
+
+                        <Typography className='english' variant='body1'>
+                            District: {feature.properties.dis}
+                        </Typography>
+
+                        <Typography className='bengali' variant='body1'>{`
+                            ${regionTranslations.Districts.District}:
+                            ${regionTranslations.Districts[feature.properties.dis]}
+                        `}</Typography>
+
+                        <Typography className='english' variant='body1'>
+                            Upazila: {feature.properties.upa}
+                        </Typography>
+
+                        <Typography className='bengali' variant='body1'>{`
+                            ${regionTranslations.Upazilas.Upazila}:
+                            ${regionTranslations.Upazilas[feature.properties.upa]}
+                        `}</Typography>
+                    </>
+                ));
+            }}
+        />
+    )
+}
