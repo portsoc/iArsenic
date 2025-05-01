512a36e7d797238596465c6d65aa434389177b11
JavaRip <up941226@myport.ac.uk>
2025-05-01 09:42:48 +0100

    remove debug log

--- app/server/src/server/repositories/prediction.repo.ts
+++ app/server/src/server/repositories/prediction.repo.ts
@@ -25,8 +25,6 @@ export const PredictionRepo: IPredictionRepo = {
                 : docData.createdAt,
         };
 
-        console.log('44444444444444444444444444444444')
-        console.log(doc)
         const prediction = PredictionSchema.parse(doc);
         return prediction;
     },
