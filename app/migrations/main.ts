79eeb839991f54dbdd1ccb6ca50d5e989b7b52d8
JavaRip <up941226@myport.ac.uk>
2025-05-02 21:39:01 +0100

    add migration for wellInUse field

--- app/migrations/main.ts
+++ app/migrations/main.ts
@@ -13,6 +13,7 @@ const lockDocRef = db.collection(migrationsCollection).doc('lock');
 const migrations = [
     () => import("./0.js"),
     () => import("./1.js"),
+    () => import("./2.js"),
 ];
 
 async function fileExists(docRef: FirebaseFirestore.DocumentReference): Promise<boolean> {
